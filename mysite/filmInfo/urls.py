e2278001c820c3677bf85c85aa29bdbdb81f7388
Maaike Maas <maaike.maas@procurios.eu>
2018-04-25 03:57:29 +0200

    Add views and finishing part 3

--- mysite/filmInfo/urls.py
+++ mysite/filmInfo/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'filmInfo'
 urlpatterns = [
     path('', views.index, name='index'),
+	path('duration/', views.duration, name='duration'),
+		path('release/', views.release, name='release'),
 ]
--- mysite/filmInfo/views.py
+++ mysite/filmInfo/views.py
@@ -1,5 +1,20 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
-
+from .models import FilmInfo
+	
 def index(request):
-    return HttpResponse("Hello. You're at the film info index.")
+    filmInfo_list = FilmInfo.objects.all()
+    context = {'filmInfo_list': filmInfo_list}
+    return render(request, 'filmInfo/index.html', context)
+	
+
+def duration(request):
+    filmInfo_list = FilmInfo.objects.order_by('-duration')
+    context = {'filmInfo_list': filmInfo_list}
+    return render(request, 'filmInfo/duration.html', context)
+
+	
+def release(request):
+    filmInfo_list = FilmInfo.objects.order_by('-releaseDate')
+    context = {'filmInfo_list': filmInfo_list}
+    return render(request, 'filmInfo/release.html', context)	
